f13c3fd265b65ad112322469a626eae5dc33440e
99002768 <72514434+99002768@users.noreply.github.com>
2020-10-09 21:44:21 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -4,15 +4,13 @@
 #include <stdio.h>
 
 #include<math.h>
-int add(int input1,int input2);
-int subtract(int input1,int input2);
-int multiply(int input1,int input2);
-int divide(int input1,int input2);
-double squareroot(double triginput);
-int modulus(int input1,int input2);
-int fact(int input1);
-double cosine(double triginput);
-double sine(double triginput);
-int power(int input1,int input2);
+int do_add(int input1,int input2);
+int do_subtract(int input1,int input2);
+int do_multiply(int input1,int input2);
+int do_divide(int input1,int input2);
+double do_squareroot(double triginput);
+int do_modulus(int input1,int input2);
+int do_fact(int input1);
+double do_cosine(double triginput);
 
 #endif
