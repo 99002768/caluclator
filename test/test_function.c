3abb173253b21dba9aced99978aafdd3ea19ba90
99002768 <72514434+99002768@users.noreply.github.com>
2020-10-09 21:48:15 +0530

    Update test_function.c

--- test/test_function.c
+++ test/test_function.c
@@ -5,8 +5,8 @@
 #define PROJECT_NAME "function"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
+void test_do_add(void);
+void test_do_subtract(void);
 
 
 /* Start of the application test */
@@ -19,8 +19,8 @@ int main() {
 /* Note: Do not edit END */
 
 /* Add your test functions in this format for testing*/
-  CU_add_test(suite, "add", test_add);
-  CU_add_test(suite, "subtract", test_subtract);
+  CU_add_test(suite, "add", test_do_add);
+  CU_add_test(suite, "subtract", test_do_subtract);
 
 
 /* Note: Do not edit START*/
@@ -36,23 +36,23 @@ int main() {
   return 0;
 }
 /* Write all the test functions */
-void test_add(void) {
-  CU_ASSERT(30 == add(10,20));
-  CU_ASSERT(40 == add(8,32));
-  CU_ASSERT(6 == add(3,3));
-  CU_ASSERT(5040 == add(2520,2520));
-  CU_ASSERT(720 == add(360,360));
+void test_do_add(void) {
+  CU_ASSERT(30 == do_add(10,20));
+  CU_ASSERT(40 == do_add(8,32));
+  CU_ASSERT(6 == do_add(3,3));
+  CU_ASSERT(5040 == do_add(2520,2520));
+  CU_ASSERT(720 == do_add(360,360));
 
   /* Dummy fail*/
-  CU_ASSERT(100 == add(95,5));
+  CU_ASSERT(100 == do_add(95,5));
 }
 
-void test_subtract(void) {
-  CU_ASSERT(100 == subtract(200,100));
-  CU_ASSERT(20 == subtract(60,40));
-  CU_ASSERT(44 == subtract(100,56));
-  CU_ASSERT(45 == subtract(50,5));
+void test_do_subtract(void) {
+  CU_ASSERT(100 == do_subtract(200,100));
+  CU_ASSERT(20 == do_subtract(60,40));
+  CU_ASSERT(44 == do_subtract(100,56));
+  CU_ASSERT(45 == do_subtract(50,5));
 
   /* Dummy fail*/
-  CU_ASSERT(0 == subtract(6,6));
+  CU_ASSERT(0 == do_subtract(6,6));
 }
